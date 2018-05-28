98558938ee195c2101532e82d8f007e5b5c07466
Elton Lin <eltonlin1998@gmail.com>
2018-05-28 23:52:38 +0100

    classifier correctly predicts test data

--- flower.py
+++ flower.py
@@ -1,7 +1,21 @@
 import numpy as np
 from sklearn.datasets import load_iris
+from sklearn import tree
 
 iris = load_iris()
-test_idx = [0, 50, 100]
+# given an array, return the 0th, 50th and 100th index and then return it when using np.delete()
+test_idx = [0, 50, 100] 
 
-print("irs =", iris)
+# training data
+train_target = np.delete(iris.target, test_idx) # corresponding labels for the training data
+# array of 4D feature vectors
+train_data = np.delete(iris.data, test_idx, axis=0) 
+
+# testing data
+test_target = iris.target[test_idx] # correct labels for the three test data points i.e. [0, 1, 2]
+test_data = iris.data[test_idx] # the three test data points' feature vectors 
+
+clf = tree.DecisionTreeClassifier() 
+clf.fit(train_data, train_target)
+print("target =", test_target)
+print("prediction =", clf.predict(test_data))
